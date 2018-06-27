815267135936a558a52c5ffa543c0c55a78a7ca9
Qi Yu <dr.yuqi@gmail.com>
2018-06-27 10:52:22 -0400

    Update vcf filter.sh

--- vcf filter.sh
+++ vcf filter.sh
@@ -5,4 +5,4 @@ vcffilter -f "DP >5" -g "GQ >10" a.vcf >a_DP5_GQ10.vcf
 vcftools --vcf a_DP5_GQ10.vcf --remove-indels --recode --recode-INFO-all --out a_DP5_GQ10_SNP_only.vcf
 
 #Subset with sample names (seperated by comma) and minimum 1 allele is non-reference.
-bcftools view -s the_samples_you_are_interested a_DP5_GQ10_SNP_only.vcf -c1 -o >a_DP5_GQ10_SNP_only_subset.vcf
+bcftools view -s the_samples_you_are_interested a_DP5_GQ10_SNP_only.vcf -c1 -o a_DP5_GQ10_SNP_only_subset.vcf
